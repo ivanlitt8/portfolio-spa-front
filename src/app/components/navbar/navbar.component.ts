e253de34b302fd92091812730b74b2d3abf07626
Iván <ivanlitt8@gmail.com>
2022-07-09 15:26:58 -0300

    actualizacion portfolio

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,29 @@
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  @ViewChild('menu',{static:true}) menu: ElementRef | undefined
+  @ViewChild('list',{static:true}) list: ElementRef | undefined
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: { target: { innerWidth: any; }; }) {
+    if(event.target.innerWidth>800){
+      console.log('AAAAAAAAAA');
+    }
+  }
+
+  ngOnInit(): void {
+    console.log(this.menu?.nativeElement)
+    console.log(this.list?.nativeElement)
+  }
+
+  // toggle(): void {
+  //   list.classList.toggle('menu_links--show');
+  // }
+
+}
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,22 +1,22 @@
 alert("app.js Works");
 
-(function(){
-    const list = document.querySelector('.menu_links');
-    const menu = document.querySelector('.menu_hamburguer');
+// (function(){
+//     const list = document.querySelector('.menu_links');
+//     const menu = document.querySelector('.menu_hamburguer');
 
-    window.addEventListener('resize', ()=>{
-        if(window.innerWidth>800){
-            deleteStyleHeight();
-            if(list.classList.contains('menu_links--show')){
-                list.classList.remove('menu_links--show');
-            }
-        }else{
-            addClick();
-        }
-    })
+//     window.addEventListener('resize', ()=>{
+//         if(window.innerWidth>800){
+//             deleteStyleHeight();
+//             if(list.classList.contains('menu_links--show')){
+//                 list.classList.remove('menu_links--show');
+//             }
+//         }else{
+//             addClick();
+//         }
+//     })
 
-    menu.addEventListener('click', ()=> list.classList.toggle('menu_links--show'));
+//     menu.addEventListener('click', ()=> list.classList.toggle('menu_links--show'));
 
-    //Aca arriba se usa @viewChild 
+//     //Aca arriba se usa @viewChild 
 
-})();
+// })();
