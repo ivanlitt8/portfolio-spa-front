9ab4a245aa02440c262a90d71110097ea5336c1e
Iván <ivanlitt8@gmail.com>
2022-07-10 20:47:00 -0300

    css a componente

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-skills',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
-  constructor() { }
+  percent:any;
+
+  constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
-  }
-
+    this.datosPortfolio.obtenerDatos().subscribe((data:any)=>{
+      this.percent= data.percent;
+    }
+  )}
 }
