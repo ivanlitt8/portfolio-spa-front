13aa7e22cfa3f553afb48075f108c22444f74371
Iván <ivanlitt8@gmail.com>
2023-01-27 23:36:30 -0300

    Carpeta BackEnd Creada

---
+++ BackEnd/src/main/java/com/portfolio/demo/IvanPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvanPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IvanPortfolioApplication.class, args);
+	}
+
+}
