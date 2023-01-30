51681d627f908bab368cbad486cf143cc6c4532a
Iván <ivanlitt8@gmail.com>
2023-01-30 00:42:38 -0300

    backend terminado, roles user u admin creados en db

--- BackEnd/src/main/java/com/portfolio/demo/Security/MainSecurity.java
+++ BackEnd/src/main/java/com/portfolio/demo/Security/MainSecurity.java
@@ -1,6 +1,23 @@
-
 package com.portfolio.demo.Security;
 
+import com.portfolio.demo.Security.Service.UserDetailsImpl;
+import com.portfolio.demo.Security.jwt.JwtEntryPoint;
+import com.portfolio.demo.Security.jwt.JwtTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/*
 import com.portfolio.demo.Security.Service.UserDetailsImpl;
 import com.portfolio.demo.Security.jwt.JwtEntryPoint;
 import com.portfolio.demo.Security.jwt.JwtTokenFilter;
@@ -32,5 +49,49 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+    
+}
+ */
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class MainSecurity {
+
+    @Autowired
+    UserDetailsImpl userDetailsServiceImpl;
+
+    @Autowired
+    JwtEntryPoint jwtEntryPoint;
+
+    @Bean
+    public JwtTokenFilter jwtTokenFilter() {
+        return new JwtTokenFilter();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(jwtEntryPoint).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeHttpRequests()
+                .requestMatchers("/**").permitAll()
+                .anyRequest().authenticated();
+
+        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
     
 }
