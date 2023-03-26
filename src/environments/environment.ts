c715094c809c9c9bdcc76873821837520b80c538
Iván <ivanlitt8@gmail.com>
2023-03-26 18:41:30 -0300

    cambios en el front

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,7 +12,7 @@ export const environment = {
     messagingSenderId: '539269684239',
     measurementId: 'G-G10Q57PVB4',
   },
-  production: false,
+  production: true,
   URL: 'https://portfolio-backend-izsb.onrender.com/'
   //URL: 'http://localhost:8080/'
 };
